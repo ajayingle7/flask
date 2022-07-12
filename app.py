a7c99e998f00e6c19c617821e0be3c8027393329
ajayingle <ajayingle109@gmail.com>
2022-07-12 15:01:27 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/home")
+def hello_world():
+    tempalte_name = "index.html"
+    return render_template(tempalte_name)
+
+
+@app.route("/contact")
+def contact():
+    return "this is contact page"
+
+@app.route("/about")
+def about():
+    return "about page"
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
